587d8f1fee2ecf6febc28e9183e284016ebb269d
leeseungmin <cldkdi1@naver.com>
2026-03-28 00:43:20 +0900

    feat: 렌더링 비동기 요청 및 생성일자 자동 기록 설정 추가

--- backend/src/main/java/kr/ac/dongeui/virtualfitting/VirtualFittingApplication.java
+++ backend/src/main/java/kr/ac/dongeui/virtualfitting/VirtualFittingApplication.java
@@ -2,8 +2,11 @@ package kr.ac.dongeui.virtualfitting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync // 비동기 백그라운드 작업 허용하는 어노테이션
+@EnableJpaAuditing
 @SpringBootApplication
 public class VirtualFittingApplication {
 	public static void main(String[] args) {
